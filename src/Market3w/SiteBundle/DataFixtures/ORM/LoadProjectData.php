31216065253fda49a221ce5961de3cd5817f43d2
heleneC <helene.cluzeau@gmail.com>
2014-07-20 19:23:28 +0200

    Fixtures project

---
+++ src/Market3w/SiteBundle/DataFixtures/ORM/LoadProjectData.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Market3w\SiteBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Market3w\SiteBundle\Entity\Project;
+
+class LoadProjectData implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+//        $presentation = "Lorem Elsass ipsum Strasbourg Verdammi id Richard Schirmeck libero, mamsell Salu bissame Coopé de Truchtersheim hopla dolor jetz gehts los hopla ornare gravida libero. condimentum Gal. porta kartoffelsalad in, ac sit barapli merci vielmols Pellentesque messti de Bischheim elit geïz turpis, DNA, senectus purus rucksack knack Hans bredele hoplageiss sit yeuh. nüdle amet sed id, commodo rossbolla wurscht Mauris kougelhopf Christkindelsmärik quam.";
+//        $duty = "Lorem Elsass ipsum Strasbourg Verdammi id Richard Schirmeck libero, mamsell Salu bissame Coopé de Truchtersheim hopla dolor jetz gehts los hopla ornare gravida libero. condimentum Gal. porta kartoffelsalad in, ac sit barapli merci vielmols Pellentesque messti de Bischheim elit geïz turpis, DNA, senectus purus rucksack knack Hans bredele hoplageiss sit yeuh. nüdle amet sed id, commodo rossbolla wurscht Mauris kougelhopf Christkindelsmärik quam. so salu schneck dignissim Morbi ch'ai consectetur hop rhoncus amet, semper Carola quam, elementum ac ornare und morbi Miss Dahlias vielmols, bissame placerat libero, tellus Heineken risus, munster schpeck adipiscing tchao bissame blottkopf, habitant varius sagittis Salut bisamme chambon mollis leverwurscht Pfourtz ! ";
+//        $work = "Lorem Elsass ipsum Strasbourg Verdammi id Richard Schirmeck libero, mamsell Salu bissame Coopé de Truchtersheim hopla dolor jetz gehts los hopla ornare gravida libero. condimentum Gal. porta kartoffelsalad in, ac sit barapli merci vielmols Pellentesque messti de Bischheim elit geïz turpis, DNA, senectus purus rucksack knack Hans bredele hoplageiss sit yeuh. nüdle amet sed id, commodo rossbolla wurscht Mauris kougelhopf Christkindelsmärik quam. so salu schneck dignissim Morbi ch'ai consectetur hop rhoncus amet, semper Carola quam, elementum ac ornare und morbi Miss Dahlias vielmols, bissame placerat libero, tellus Heineken risus, munster schpeck adipiscing tchao bissame blottkopf, habitant varius sagittis Salut bisamme chambon mollis leverwurscht Pfourtz ! lotto-owe s'guelt eget non mänele Oberschaeffolsheim ullamcorper Oberschaeffolsheim météor ftomi! flammekueche baeckeoffe knepfle sed picon bière Yo dû. amet lacus gal Spätzle auctor, Racing. Wurschtsalad tristique hopla eleifend vulputate suspendisse geht's ante nullam Gal ! Huguette réchime gewurztraminer tellus wie pellentesque et Chulia Roberstau Kabinetpapier non turpis Chulien leo kuglopf aliquam dui leo schnaps sit hopla .";
+//        
+//        for($i=0; $i<10; $i++){
+//            $project = new Project();
+//            $project->setName("Project_".$i);
+//            $project->setPresentation($presentation);
+//            $project->setUrl("http://www.google.fr");
+//            $project->setDuty($duty);
+//            $project->setWork($work);
+//            
+//            $manager->persist($project);
+//        }
+//               
+//        $manager->flush();
+    }
+}

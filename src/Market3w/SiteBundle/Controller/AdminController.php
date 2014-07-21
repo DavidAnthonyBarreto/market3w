8b0e1041a7d10a23c0a1c357494d2d2259faaf8c
heleneC <helene.cluzeau@gmail.com>
2014-07-21 13:06:14 +0200

    admin

---
+++ src/Market3w/SiteBundle/Controller/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+namespace Market3w\SiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();    
+    }
+    
+    
+}

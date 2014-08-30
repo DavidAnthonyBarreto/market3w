7b22db9aa3ddd1120161050f7ecf9305ab72041e
heleneC <helene.cluzeau@gmail.com>
2014-08-30 20:34:45 +0200

    diverses améliorations

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Market3w\SiteBundle\Market3wSiteBundle(),
         );
 
